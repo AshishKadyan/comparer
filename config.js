1fb50da871f48e37d97e52babea954888e07d652
Ashish <ashish.kadyan@comprotechnologies.com>
2018-07-24 17:30:41 +0530

    updated code

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
     // path1: "D:/Comparer/resources/5B.03.T1 - Copy",
     // path2: "D:/Comparer/resources/5B.03.T1",
    
-    path1: "D:/SIMS/Modules/sim5service/XMLs/TaskXmls2016/go/ac/01/1a.01.a1",
-    path2: "D:/SIMS/Modules/sim5service/XMLs/TaskXmls2016/go/ac/01/1a.01.t1",
+    path1: "D:/Comparer/resources/1a.08.a1",
+    path2: "D:/Comparer/resources/1a.08.t1",
     dest: "D:/Comparer/resources/Destination/assets"
   }
 };
